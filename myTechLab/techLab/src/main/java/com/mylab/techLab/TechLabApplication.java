7b0f8cf47af71f7800799722014ee46f2094e41e
dmstjd0214@naver.com <dmstjd0214@naver.com>
2024-07-22 09:33:00 +0900

    프로젝트 생성

---
+++ myTechLab/techLab/src/main/java/com/mylab/techLab/TechLabApplication.java
@@ -0,0 +1,13 @@
+package com.mylab.techLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechLabApplication.class, args);
+	}
+
+}
